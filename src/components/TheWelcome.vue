6712b998ada6a6287a83f1e493a308226addd3ae
Manato Yamashita <tcumanato@gmail.com>
2023-08-29 13:17:05 +0900

    fixed dist

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="welcome">
-    <h1>How you feel? I’m so so so…</h1>
+    <h1>How you feel? I'm so so so…</h1>
     <h2>Manato Yamashita</h2>
     <img src="@/assets/ym.webp" alt="Manato Yamashita" id="profile-img">
     <p>
@@ -13,15 +13,23 @@
       MANAPURAZA.COM is implemented <strong>SPA（Single Page Application）</strong> by <strong><a href="https://ja.vitejs.dev/" target="_blank">Vite</a></strong><fa :icon="['fas', 'arrow-up-right-from-square']" class="fa" /> with
        <strong><a href="https://ja.vuejs.org/" target="_blank">Vue.js</a></strong><fa :icon="['fas', 'arrow-up-right-from-square']" class="fa" />!
     </p>
-    <p>In my classes, I learned the basic theories of <strong>ICT assessments</strong> and <strong>multimedia</strong>,
-        and also did team development using <strong>programming</strong>, from requirement definition to video production.
-          Furthermore, as the representative of a group with more than 100 members that creates digital contents using digital tools,
-          I did <strong>video production</strong>, <strong>graphics design</strong>, <strong>llustration</strong>, <strong>programming</strong>, and <strong>iweb design</strong>.
-            I also enjoy making animations as a hobby, and I <strong>love all of creative</strong> activities in general.</p>
+    <p>
+      In my classes, I learned the basic theories of <strong>ICT assessments</strong> and <strong>multimedia</strong>,
+      and also did team development using <strong>programming</strong>, from requirement definition to video production.
+      Furthermore, as the representative of a group with more than 100 members that creates digital contents using digital tools,
+      I did <strong>video production</strong>, <strong>graphics design</strong>, <strong>llustration</strong>, <strong>programming</strong>, and <strong>iweb design</strong>.
+      I also enjoy making animations as a hobby, and I <strong>love all of creative</strong> activities in general.
+    </p>
   </div>
 </template>
 
 <style scoped>
+.welcome {
+  padding: 1rem;
+}
+p {
+  font-size: .9rem;
+}
 img#profile-img {
   width: 15vw;
   border-radius: 5px;
