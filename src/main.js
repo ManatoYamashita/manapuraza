e321b3594d44dfcad00281833a4c329921cdabe5
yamashita <g2172117@tcu.ac.jp>
2023-11-23 04:03:08 +0900

    [add]impremeted metaball by THREE-JS

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Navbar from './components/navbar.vue'
+import MetaBall from './components/MetaBall.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
@@ -13,10 +14,13 @@ library.add(faArrowUpRightFromSquare)
 
 const app = createApp(App)
 const navbar = createApp(Navbar)
+const metaball = createApp(MetaBall)
 app.component('fa', FontAwesomeIcon)
 
 navbar.use(router)
+metaball.use(router)
 app.use(router)
 
 app.mount('#app')
 navbar.mount('#navbar')
+metaball.mount('#back')
